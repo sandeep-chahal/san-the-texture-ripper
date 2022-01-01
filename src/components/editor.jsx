9799d4d5669034ea75839f85db2b3cab5eb92707
sandeep-chahal <sandeepchahal573@gmail.com>
2022-01-01 14:13:07 +0530

    moved cropbox to its own component

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -2,11 +2,11 @@ import React, { useRef, useEffect, useState } from "react";
 import { useMainStore } from "../store";
 import { useEditorStore } from "../store/editor";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import * as d3 from "d3";
 import fx from "glfx";
 import { uniqueId } from "../utils";
 import DeleteSvg from "../components/svg/delete-svg";
 import AddSvg from "../components/svg/add-svg";
+import CropBox from "./cropbox";
 
 const tempCanvas = document.createElement("canvas");
 const tempCanvasCtx = tempCanvas.getContext("2d");
@@ -16,10 +16,9 @@ const Editor = () => {
 	const wrapperRef = useRef(null); // transform wrapper(zoom, pan, pinch)
 	const parentRef = useRef(null); // parent/container div
 	const [disabled, setDisabled] = useState(true); // if panning and zooming is disabled / ctrl key is pressed or not
-	const svg = useRef(null); // cropbox svg
 	const canvas = useRef(); // canvas
 	const scale = useRef(1); // zoom scale
-	const line = useRef(null); // cropbox lines/grid lines
+	const cropBox = useRef(null);
 
 	const { file, setResults, warpRealTime } = useMainStore();
 	const {
@@ -43,7 +42,7 @@ const Editor = () => {
 		glfxCanvas.current.draw(texture.current).update();
 
 		if (!activeLayer) addLayer();
-		drawCropBox();
+		if (cropBox.current) cropBox.current.drawCropBox();
 	};
 
 	// when new file is imported
@@ -78,7 +77,7 @@ const Editor = () => {
 				// console.log("zoom in");
 				wrapperRef.current.zoomIn();
 				scale.current += 0.5;
-				drawCropBox();
+				if (cropBox.current) cropBox.current.drawCropBox();
 			} else if (
 				(isMouseOver.current && e.keyCode === 109) ||
 				e.keyCode === 189
@@ -86,7 +85,7 @@ const Editor = () => {
 				// console.log("zoom out");
 				wrapperRef.current.zoomOut();
 				scale.current -= 0.5;
-				drawCropBox();
+				if (cropBox.current) cropBox.current.drawCropBox();
 			}
 		};
 		const onKeyUp = (e) => {
@@ -146,103 +145,8 @@ const Editor = () => {
 	}, []);
 
 	useEffect(() => {
-		drawCropBox();
+		if (cropBox.current) cropBox.current.drawCropBox();
 	}, [activeLayer, warpRealTime]);
-
-	// draw d3 svg cropbox
-	const drawCropBox = () => {
-		if (file === null || activeLayer == null || !layers.current[activeLayer])
-			return;
-		svg.current.selectAll("*").remove();
-		const { radius, strokeWidth, opacity } = getSvgSize();
-		line.current = svg.current
-			.selectAll(".line")
-			.data(new Array(10).fill(null))
-			.enter()
-			.append("path")
-			.attr("class", "line")
-			.attr("stroke-width", strokeWidth * 0.5)
-			.attr("stroke-opacity", opacity)
-			.attr("stroke", "#00ACED");
-
-		svg.current
-			.selectAll(".handle")
-			.data(layers.current[activeLayer].points)
-			.enter()
-			.append("circle")
-			.attr("class", `handle cursor-move`)
-			.attr("fill", "transparent")
-			.attr("stroke-width", strokeWidth * 0.8)
-			.attr("stroke-opacity", opacity)
-			.attr("r", radius)
-			.attr("stroke", "#C27224")
-			.attr("data-index", (d, i) => i)
-			.attr("transform", function (d) {
-				return "translate(" + d + ")";
-			})
-			.call(
-				d3
-					.drag()
-					// when handles(circles) are dragged
-					.on("drag", function (d) {
-						// check if new position is within canvas
-						if (
-							d.x > 0 &&
-							d.x < canvas.current.width &&
-							d.y > 0 &&
-							d.y < canvas.current.height
-						) {
-							// update points
-							d3.select(this).attr(
-								"transform",
-								"translate(" + d.x + "," + d.y + ")"
-							);
-							layers.current[activeLayer].points[
-								d3.select(this).attr("data-index")
-							] = [d.x, d.y];
-							updatePerspectiveGrid();
-							if (!warpRealTime) return;
-							updateResultGLFX();
-						}
-					})
-					.on("end", () => {
-						updateResultGLFX();
-					})
-			);
-
-		updatePerspectiveGrid();
-		// draw cropbox lines/grid
-		function updatePerspectiveGrid() {
-			const points = layers.current[activeLayer].points;
-			const verticalLines1 = getMidPointArray(points[0], points[3]);
-			const verticalLines2 = getMidPointArray(points[1], points[2]);
-			const horizontalLines1 = getMidPointArray(points[0], points[1]);
-			const horizontalLines2 = getMidPointArray(points[3], points[2]);
-			const linePoints1 = [...verticalLines1, ...horizontalLines1];
-			const linePoints2 = [...verticalLines2, ...horizontalLines2];
-			line.current._groups[0].forEach((line, index) => {
-				line.setAttribute(
-					"d",
-					`M${linePoints1[index][0]},${linePoints1[index][1]}L${linePoints2[index][0]},${linePoints2[index][1]}`
-				);
-			});
-		}
-
-		// get mid point of a line/two points
-		function midPoint(x1, y1, x2, y2) {
-			return [(x1 + x2) / 2, (y1 + y2) / 2];
-		}
-
-		function getMidPointArray(p1, p2) {
-			const points = [];
-			points.push(p1);
-			points.push(p2);
-			points.push(midPoint(p1[0], p1[1], p2[0], p2[1]));
-			points.push(midPoint(p1[0], p1[1], points[2][0], points[2][1]));
-			points.push(midPoint(p2[0], p2[1], points[2][0], points[2][1]));
-			return points;
-		}
-	};
 
 	// perform warping using glfx library
 	const updateResultGLFX = () => {
@@ -437,21 +341,23 @@ const Editor = () => {
 				onZoomStop={(ref) => {}}
 				onZoom={(ref) => {
 					scale.current = ref.state.scale;
-					drawCropBox();
+					if (cropBox.current) cropBox.current.drawCropBox();
 				}}
 			>
 				<TransformComponent wrapperStyle={{ width: "100%", height: "100%" }}>
 					<div>
 						{/* actual canvas */}
 						<canvas ref={canvas}></canvas>
-						{/* cropbox */}
-						<svg id="svg" className="absolute top-0 left-0 w-full h-full">
-							<g
-								ref={(ref) => {
-									svg.current = d3.select(ref);
-								}}
-							></g>
-						</svg>
+						<CropBox
+							canvas={canvas}
+							layers={layers}
+							updateResultGLFX={updateResultGLFX}
+							ref={cropBox}
+							file={file}
+							activeLayer={activeLayer}
+							getSvgSize={getSvgSize}
+							warpRealTime={warpRealTime}
+						/>
 					</div>
 				</TransformComponent>
 			</TransformWrapper>
